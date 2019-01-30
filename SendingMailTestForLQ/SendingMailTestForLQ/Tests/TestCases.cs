aaae4991b0c93ac4cb70befc5c2c1dc5802d203e
Damir <pt7mk@yandex.ru>
2019-01-30 16:37:16 +0700

    rurururu

--- SendingMailTestForLQ/SendingMailTestForLQ/Tests/TestCases.cs
+++ SendingMailTestForLQ/SendingMailTestForLQ/Tests/TestCases.cs
@@ -11,7 +11,7 @@ namespace SendingMailTestForLQ.Tests
         private static MailPageHelper MailPageHelper = new MailPageHelper();
 
         [Test] 
-        public static void LoginOnMailruru()
+        public static void LoginOnMailrurururu()
         {
             string email = "test.account.damir@mail.ru";
             string password = "q123123a";
